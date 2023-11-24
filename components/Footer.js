881c2354404059c3bca4e93e364e8907f967a48e
Graeme Paterson <ghpaterson@gmail.com>
2023-11-24 15:01:03 +0000

    footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="w-full flex justify-between py-10 ">
+      <div>
+        <p>Graeme Paterson</p>
+        <p>ghpaterson@gmail.com</p>
+        <p>07481844603</p>
+      </div>
+      <div className="flex items-end">
+        <p>&copy; Graeme Paterson 2023</p>
+      </div>
+    </footer>
+  );
+}
